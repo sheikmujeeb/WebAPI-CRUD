7a942d986d31a83935e1a6a4bb0c32ba560d5cc0
MUJEE <MUJEE@DESKTOP-S05PO2V>
2024-05-26 15:45:18 +0530

    CRUD operation works

--- CRUD-WebAPIclasslibrary/BusRespos.cs
+++ CRUD-WebAPIclasslibrary/BusRespos.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
+using System.Data.SqlClient;
+using Dapper;
 
 namespace CRUD_WebAPIclasslibrary
 {
@@ -37,7 +39,82 @@ namespace CRUD_WebAPIclasslibrary
             }
             public IEnumerable<Busdetails>SPshowall()
             {
+                try
+                {
+                    var showall = $"select BusID,BusName,DriverMobilennumber,StartPoint,Destination,Fair,NoofPassenger from Busdetails ";
+                    connection.open();
+                    var match = connection.Query<Busdetails>(showall);
+                    connection.close();
+                    return match;
+                }
+                catch(Sqlexception ex)
+                {
+                    throw;
+                }
+                catch(Exception)
+                {
+                    throw;
+                }
 
+            }
+            public void SPupdate(string start ,string end ,int far ,int ticket)
+            {
+                try
+                {
+                    var update = $"exec SPupdate '{start}''{end}''{far}''{ticket}';";
+                    Console.WriteLine("Updated Successfully");
+                    connection.open();
+                    connection.execute(update);
+                    connection.close();
+
+                }
+                catch (Sqlexception ex)
+                {
+                    throw;
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+            }
+            public void SPremove(string busname)
+            {
+                try
+                {
+                    var delete = $"exec SPremove '{busname}'";
+                    Console.WriteLine("Deleted Sucessfully");
+                    connection.open();
+                    connection.execute(delete);
+                    connection.close();
+                }
+                catch (Sqlexception ex)
+                {
+                    throw;
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+            }
+            public IEnumerable<Busdetails>SPsearch(string name)
+            {
+                try
+                {
+                    var search = $"exec SPsearch '{name}'";
+                    connection.open();
+                    var match = connection.Query<Busdetails>(search);
+                    connection.close();
+                    return match;
+
+                }
+                catch (Sqlexception ex)
+                {
+                    throw;
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
             }
         }
         
