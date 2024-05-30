56c82d50c6d43e9ee5a4bc6bdd74c3d456b5bd3a
MUJEE <MUJEE@DESKTOP-S05PO2V>
2024-05-30 19:19:48 +0530

    test

---
+++ ConsoleApp1/revrse.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class revrse
+    {
+        public void reverse(string a)
+        {
+            Console.WriteLine("Reverse");
+             string temp ;
+            string temp2;
+            string result;
+            
+            for(int i=0;i<a.Length; i++)
+            {
+
+                temp = Convert.ToString(a[i]);
+                temp2 = temp;
+
+                result = temp + temp2;
+                
+                Console.WriteLine(result);
+                
+
+
+
+            }
+            
+
+        }
+    }
+}
