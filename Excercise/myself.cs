ef4c0805e8a637c3a3400fb7420ad5cafe1c72bf
MUJEE <MUJEE@DESKTOP-S05PO2V>
2024-06-03 17:07:23 +0530

    push

---
+++ Excercise/myself.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excercise
+{
+    class myself
+    {
+        public void wordreverse()
+        {
+            string str;
+            string a = "";
+            Console.WriteLine("Enter the string:");
+            str = Console.ReadLine();
+            str = str.ToLower();
+            for(int i=str.Length;i>=0;i++)
+            {
+                a = a + str[i];
+                
+            }
+            Console.WriteLine(a);
+        }
+    }
+}
